bc69acb359f9456b10ff9dbdd3dafff106c080ff
Felipe Dzul <felipe.dzul.m@gmail.com>
2020-12-03 19:00:30 -0600

    update the globals

--- R/globals.R
+++ R/globals.R
@@ -3,4 +3,7 @@ utils::globalVariables(c('%>%', "ver_blocks", "MUNICIPIO", "LOCALIDAD",
                          "Municipio", "Semana.Epidemiologica", "Manzana",
                          "sector", "sec", "man", "n", 'Cobertura.en.Manzana',
                          'Localidad', 'sec_manz', 'NOMGEO', 'AMBITO', 'path_ine',
-                         'geometry', 'week'))
+                         'geometry', 'week', 'Jurisdiccion', 'Casas.Revisadas',
+                         'Casas.Trabajadas', 'desc', 'Casas Trabajadas hasta la Semana',
+                         'mean_count', 'NombreMunicipio', "year", "mun",
+                         "Huevos", "quantil", "Huevecillos. "))
