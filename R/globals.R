461a9c7b9a29bb6e599ba521ed69521170811abe
Felipe Dzul <felipe.dzul.m@gmail.com>
2020-12-03 18:45:54 -0600

    Add the global variables

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c('%>%', "ver_blocks", "MUNICIPIO", "LOCALIDAD",
+                         "SECCION", "LOCALIDAD", "SECCION", "MANZANA",
+                         "Municipio", "Semana.Epidemiologica", "Manzana",
+                         "sector", "sec", "man", "n", 'Cobertura.en.Manzana',
+                         'Localidad', 'sec_manz', 'NOMGEO', 'AMBITO', 'path_ine',
+                         'geometry', 'week'))
