813ea2a2d5a9ec954978efdefc8bf34fd7421d5e
Felipe Dzul <felipe.dzul.m@gmail.com>
2021-05-20 22:39:17 -0500

    This commit fix the correct format object. The sf to df

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
 #' Dengue cases by states for epidemiological channel.
 #'
-#' A dataset containing dengue cases by 25, 50, & 75 percentil
+#' A dataset containing  the 25, 50, & 75 percentil of dengue cases.
 #'
-#' @format A sf object with 1545 rows and 5 variables:
+#' @format A df object with 1545 rows and 5 variables:
 #' \describe{
 #'   \item{DES_EDO_RES}{is the name of state.}
 #'   \item{SEM}{Epidemiological week.}
