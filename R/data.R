86e1aee0b7548d65ac3dcab89a84d7a8facc0883
Felipe Dzul <felipe.dzul.m@gmail.com>
2021-05-20 22:31:48 -0500

    This commit update the package and add the den_epichannel data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Dengue cases by states for epidemiological channel.
+#'
+#' A dataset containing dengue cases by 25, 50, & 75 percentil
+#'
+#' @format A sf object with 1545 rows and 5 variables:
+#' \describe{
+#'   \item{DES_EDO_RES}{is the name of state.}
+#'   \item{SEM}{Epidemiological week.}
+#'   \item{q25}{percentil 25.}
+#'   \item{q50}{percentil 50.}
+#'   \item{q75}{percentil 75.}
+#'   ...
+#' }
+"den_epichannel"
